be28456940290967edefd1274cc6d2d6dce44823
HIMAL L R <117270050+himallr@users.noreply.github.com>
2023-12-04 13:32:00 +0530

    Update SignIn.js

--- vegmart/src/signinpages/SignIn.js
+++ vegmart/src/signinpages/SignIn.js
@@ -11,7 +11,7 @@ function SignIn() {
     const [confirm, setConfirmPassword] = useState("");
 
     const submitOnClick = () => {
-        Axios.post('https://veg-mart-5u48.vercel.app/api/create',
+        Axios.post('https://veg-mart-server.vercel.app/api/create',
             { user: user, email: email, password: password, confirm_password: confirm }).then(() => {
                 console.log("submitted");
             });
