d3a40dbbc338a9a1409789ef118d4f9fe9efa65c
HIMAL L R <117270050+himallr@users.noreply.github.com>
2023-12-04 13:24:37 +0530

    Update grocery.js

--- vegmart/src/card1page/grocery.js
+++ vegmart/src/card1page/grocery.js
@@ -7,7 +7,7 @@ function Grocery(props) {
 
     useEffect(() => {
         const getItems = async () => {
-            const res = await fetch('http://localhost:3001/groceries');
+            const res = await fetch('https://veg-mart-5u48.vercel.app/groceries');
             const getdata = await res.json();
             setItems(getdata);
         }
